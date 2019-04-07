aad47a608198fb605d97b53bbdeeb00535308f56
Kyle Brodie <kylecbrodie@gmail.com>
2019-04-07 12:33:03 -0700

    Change whitespace and add comments to problem 80

--- problem_80.py
+++ problem_80.py
@@ -7,6 +7,7 @@ class TreeNode:
 	def __repr__(self):
 		return f"TreeNode(val={self.val})"
 
+# also LeetCode 104
 class Solution:
 	def maxDepth(self, root: TreeNode) -> TreeNode:
 		def _recursiveDepth(root: TreeNode, depth: int) -> (int, TreeNode):
@@ -18,9 +19,8 @@ class Solution:
 				curr = max(curr, left, right, key=lambda t: t[0])
 			return curr
 		ans = _recursiveDepth(root, 0)
+		# LeetCode 104 wants the depth number so return ans[0] instead
 		return ans[1]
-			
-
 
 soln = Solution()
 root = TreeNode('a')
